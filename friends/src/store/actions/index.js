8bbfcd46e315305de2417997a4c86b36c24fd760
Olivia Osborn <olioz95@gmail.com>
2019-01-31 16:26:25 -0500

    filled in action creator for fetching friends data

--- friends/src/store/actions/index.js
+++ friends/src/store/actions/index.js
@@ -1,5 +1,21 @@
+import axios from "axios";
+
 export const FETCHING_FRIENDS_START = "FETCHING_FRIENDS_START";
 export const FETCHING_FRIENDS_SUCCESS = "FETCHING_FRIENDS_SUCCESS";
 export const FETCHING_FRIENDS_FAILURE = "FETCHING_FRIENDS_FAILURE";
 
 
+export const getFriends = () => dispatch => {
+    dispatch({type: FETCHING_FRIENDS_START});
+    axios
+        .get("http://localhost:5000/api/friends")
+        .then(response => {
+            console.log(response)
+            dispatch({type: FETCHING_FRIENDS_SUCCESS, payload: response.data})
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch({type: FETCHING_FRIENDS_FAILURE, payload: error})
+        })
+}
+    
